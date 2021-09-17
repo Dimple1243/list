fa0f9cc1fc5833ba4f2ef79c520167cd6840b129
Dimple1243 <89607801+Dimple1243@users.noreply.github.com>
2021-09-17 15:07:22 +0530

    Add files via upload

---
+++ 2sramax.py.py
@@ -0,0 +1,17 @@
+numbers=[50, 40, 90, 70,12, 5, 10, 200]
+i=0
+max=0
+while i<len(numbers):
+    if numbers[i]>max:
+        max=numbers[i]
+    i=i+1
+print(max)
+second_max = 0
+i=0
+while i<len(numbers):
+    if numbers[i]>second_max:
+        if numbers[i]!=max:
+            second_max=numbers[i]
+    i=i+1
+print(second_max)   
+
