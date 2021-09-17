6769fbaee1366c9af826a5bff548549716e2a19a
Dimple1243 <89607801+Dimple1243@users.noreply.github.com>
2021-09-17 15:13:48 +0530

    Add files via upload

---
+++ x.py
@@ -0,0 +1,26 @@
+numbers = [50, 40, 23, 2, 56, 12, 5, 10, 7]
+i=0
+max=0
+while i<len(numbers):
+    if numbers[i]<max:
+        max=numbers[i]
+    i=i+1
+print(max)
+second_max=0
+i=0
+while i<len(numbers):
+    if numbers[i]<second_max:
+        if numbers[i]!=max:
+            second_max=numbers[i]
+    i=i+1
+print(second_max)    
+
+
+
+
+
+
+
+
+
+
